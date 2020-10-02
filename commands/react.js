53f5e2ca159f7cac51db7b2a1f0ec65844e5913b
DQuinn97 <quinten2407@gmail.com>
2020-10-02 13:31:00 +0200

    2.0.0 fix 2

--- commands/react.js
+++ commands/react.js
@@ -1,44 +1,40 @@
 const commandName = "react";
 
 function run(message, args) {
-    //command add/remove message-id emoji/all role
-    let choice = args.shift().toLowerCase();
-    switch (choice) {
-        case "add":
-            return require(`./addreact.js`).run(message, args);
-            break;
-        case "remove":
-        case "delete":
-            return require(`./delreact.js`).run(message, args);
-            break;
-        case "override":
-            if (
-                args[0] == "all" &&
-                require(`../protocols/staffcheck.js`).demand(message, 2)
-            ) {
-                const fs = require("fs");
-                let obj = {
-                    channels: []
-                };
+    //command role emoji
 
-                fs.writeFile(`files/react_cache.json`, JSON.stringify(obj), err => {
-                    if (err) return console.error(err.message);
-                    console.log(`Override succeeded; all reacts removed!`);
-                });
-                require(`../protocols/fileLog.js`).demand(`files/react_cache.json`, 3, "Override succeeded; all reacts removed!");
-            } else {
-                return console.log(`Override failed..`);
-            }
-            break;
-        default:
-            return console.log(`Cannot '${choice}' a react!`);
-            break;
+    if (!args[0] || (!message.guild.roles.cache.find(role => role.id == args[0]) && !message.mentions.roles)) return console.log(`Not a role`);
+
+    let role =
+        message.mentions.roles.first() ||
+        message.guild.roles.cache.find(role => role.id == args[0]);
+    let emoji = args[1] || undefined;
+
+    if (role.position >= message.member.roles.highest.position && message.member.id != message.guild.ownerID) return console.log(`Cannot assign role higher than member's highest role`);
+
+    let embed = {
+        "title": `Reaction Role - ${emoji ? "multi" : "single"}`,
+        "color": 13064196,
+        "fields": [{
+            "name": `---------------------------`,
+            "value": `${role} | ${emoji ? emoji : "React any emoji"}`
+            }]
     }
+    if (!emoji) {
+        message.channel.send({
+            "embed": embed
+        })
+    } else {
+        message.channel.send({
+            "embed": embed
+        }).then(msg => msg.react(emoji));
+    }
+    return true;
 }
 
 function help(message) {
     return message.channel.send(
-        `${commandName}: Add/remove a reaction/role to the watchlist.\n\`${process.env.PREFIX}${commandName} <add/remove> <message-id> <emoji/all> <role>\``
+        `${commandName}: Create a new reaction role. Adding an emoji allows for multiple reaction roles per post, with each role bound to a specific emoji. Without emoji only a single role can be given per post, but any emoji can be reacted to get it.\n\`${process.env.PREFIX}${commandName} <role> <emoji>\``
     );
 }
 module.exports.run = run;
